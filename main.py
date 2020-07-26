776beed1d02211b44b65e47844cb6b13f16a0479
lenya1567 <55053376+lenya1567@users.noreply.github.com>
2020-07-26 18:24:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from modules.Components.src.Button as btn
 from modules.Components.src.Label as lab
 
-Button = btn
-Label = lab
+class export:
+  Button = btn
+  Label = lab
+
+def main():
+  return export()
