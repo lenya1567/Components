3988db0d14fc58ee9f7170c7c78391de21314400
lenya1567 <55053376+lenya1567@users.noreply.github.com>
2020-07-26 17:38:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-from modules.Components.src import Button as btn
-from modules.Components.src import Label as lbl
-from modules.Components.src import ObjectInterface as interface
-from modules.Components.src import TextField as tfield
-
-class export:
-  Button = btn
-  Label = lbl
-  TextField = tfield
+from modules.Components.src import Button
+from modules.Components.src import Label
+from modules.Components.src import TextFiel
