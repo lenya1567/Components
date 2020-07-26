f312a70f353563fd784a1944d85e4cc79ef6b734
lenya1567 <55053376+lenya1567@users.noreply.github.com>
2020-07-26 17:06:41 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from modules.Components import Button as btn
+from modules.Components import Label as lbl
+from modules.Components import ObjectInterface as interface
+from modules.Components import TextField as tfield
+
+class export:
+  Button = btn
+  Label = lbl
+  ObjectInterface = interface
+  TextField = tfield
